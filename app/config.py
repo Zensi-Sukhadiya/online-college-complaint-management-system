19210d2938f11e5405edbb01c16dcbfdb59605e1
Zensi-Sukhadiya <sukhadiyazensi26@gmail.com>
2026-07-09 12:06:12 +0530

    Configure PostgreSQL support

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "this_is_a_secret_key"
 
-    SQLALCHEMY_DATABASE_URI = "sqlite:///college.db"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///college.db"
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
