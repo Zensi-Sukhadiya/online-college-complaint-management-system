2c554733df742ae5f64121cf5c1fd777639c7f21
Zensi-Sukhadiya <sukhadiyazensi26@gmail.com>
2026-07-09 12:08:38 +0530

    Configure PostgreSQL support

--- app/config.py
+++ app/config.py
@@ -3,10 +3,15 @@ import os
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "this_is_a_secret_key"
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///college.db"
+    database_url = os.environ.get("DATABASE_URL")
+
+    if database_url:
+        database_url = database_url.replace("postgres://", "postgresql://", 1)
+
+    SQLALCHEMY_DATABASE_URI = database_url or "sqlite:///college.db"
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     UPLOAD_FOLDER = "app/static/uploads"
-    
-    MAX_CONTENT_LENGTH = 5 * 1024 * 1024  # 5 MB
+
+    MAX_CONTENT_LENGTH = 5 * 1024 * 1024
